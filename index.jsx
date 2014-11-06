a07b7d615735443fb1e0d0efe1027c6e32bbcc1a
Huei <huei90@gmail.com>
2014-11-06 12:08:51 +0800

    Support jsx and js code

---
+++ index.jsx
@@ -0,0 +1,32 @@
+/**
+ * @jsx React.DOM
+ */
+'use strict';
+
+var React = require('react');
+
+var component = React.createClass({
+
+  render: function() {
+
+    var containerStyle = {
+      height: '10px'
+    };
+
+    var progressStyle = {
+      backgroundColor: this.props.color || '#0BD318',
+      width: (this.props.completed || 0) + '%',
+      transition: "width 200ms",
+      height: '10px'
+    };
+
+    return (
+      <div className="progressbar-container" style={containerStyle}>
+        <div className="progressbar-progress" style={progressStyle}>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = component;
