8288013fd64b8edf8a32349c848c84e6733082ce
abdennour <mail@abdennoor.com>
2017-07-29 12:22:06 +0300

    feat: Upgrade to React 15 and Upgrade Enzyme accordingly

--- src/index.js
+++ src/index.js
@@ -1,29 +1,34 @@
 import React from 'react';
-
-const PropTypes = {
-  completed: ((props, propName) => {
-    if (typeof props[propName] !== 'number')
-      return Progress.throwError('Invalid Props: "completed" should ∈ ℝ ');
-    if( props[propName] < 0 || props[propName] > 100) {
-      return Progress.throwError('Invalid Props: "completed" should be between 0 and 100' );
-    }
-  }),
-  color: React.PropTypes.string,
-  animation: React.PropTypes.number,
-  height: React.PropTypes.oneOfType([
-    React.PropTypes.string,
-    React.PropTypes.number
-  ])
-};
-
-const defaultProps = {
-  completed: 0,
-  color: '#0BD318',
-  animation: 200,
-  height: 10
-};
+import PropTypes from 'prop-types';
 
 class Progress extends React.Component {
+
+  static propTypes = {
+    completed: ((props, propName) => {
+      if (typeof props[propName] !== 'number')
+        return Progress.throwError('Invalid Props: "completed" should ∈ ℝ ');
+      if( props[propName] < 0 || props[propName] > 100) {
+        return Progress.throwError('Invalid Props: "completed" should be between 0 and 100' );
+      }
+    }),
+    color: PropTypes.string,
+    animation: PropTypes.number,
+    height: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number
+    ])
+  }
+
+  static defaultProps = {
+    completed: 0,
+    color: '#0BD318',
+    animation: 200,
+    height: 10
+  }
+
+  static throwError() {
+    return new Error(...arguments);
+  }
 
   render () {
     const {color, completed, animation, height, className, children, ...rest} = this.props;
@@ -40,12 +45,6 @@ class Progress extends React.Component {
       </div>
     );
   }
-};
-
-Progress.propTypes = PropTypes;
-Progress.throwError = function() {
-  return new Error(...arguments);
-};
-Progress.defaultProps = defaultProps;
+}
 
 export default Progress;
