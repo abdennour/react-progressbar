5228ef5e32c378ebf5eec8360eba18626c393ff5
Luke Sanwick <lsanwick@gmail.com>
2018-03-22 10:08:28 -0700

    [Prettier] Format codebase

--- src/index.js
+++ src/index.js
@@ -2,46 +2,44 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class Progress extends React.Component {
-
   static propTypes = {
-    completed: ((props, propName) => {
+    completed: (props, propName) => {
       if (typeof props[propName] !== 'number')
         return Progress.throwError('Invalid Props: "completed" should ∈ ℝ ');
-      if( props[propName] < 0 || props[propName] > 100) {
-        return Progress.throwError('Invalid Props: "completed" should be between 0 and 100' );
+      if (props[propName] < 0 || props[propName] > 100) {
+        return Progress.throwError('Invalid Props: "completed" should be between 0 and 100');
       }
-    }),
+    },
     color: PropTypes.string,
     animation: PropTypes.number,
-    height: PropTypes.oneOfType([
-      PropTypes.string,
-      PropTypes.number
-    ])
-  }
+    height: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
+  };
 
   static defaultProps = {
     completed: 0,
     color: '#0BD318',
     animation: 200,
-    height: 10
-  }
+    height: 10,
+  };
 
   static throwError() {
     return new Error(...arguments);
   }
 
-  render () {
+  render() {
     const {color, completed, animation, height, className, children, ...rest} = this.props;
     const style = {
       backgroundColor: color,
       width: completed + '%',
       transition: `width ${animation}ms`,
-      height: height
+      height: height,
     };
 
     return (
-      <div className={className || "progressbar-container"} {...rest}>
-        <div className="progressbar-progress" style={style}>{children}</div>
+      <div className={className || 'progressbar-container'} {...rest}>
+        <div className="progressbar-progress" style={style}>
+          {children}
+        </div>
       </div>
     );
   }
--- test/spec.js
+++ test/spec.js
@@ -1,41 +1,37 @@
 import 'jsdom-global/register';
 import React from 'react';
-import {
-  mount,
-  shallow
-} from 'enzyme';
+import {mount, shallow} from 'enzyme';
 import expect from 'expect';
 import sinon from 'sinon';
 
 import Progress from '../src';
 
-const rgb2hex = (rgb) => {
-  if (rgb.search("rgb") === -1) return rgb;
+const rgb2hex = rgb => {
+  if (rgb.search('rgb') === -1) return rgb;
   rgb = rgb.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+))?\)$/);
-  const hex = (x) => ("0" + parseInt(x).toString(16)).slice(-2);
-  return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
+  const hex = x => ('0' + parseInt(x).toString(16)).slice(-2);
+  return '#' + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
 };
 
-const noCase = (string) => string.toLowerCase();
+const noCase = string => string.toLowerCase();
 
 const getRange = (from, to) => {
-   if(from === to ) return [from];
-   if (from > to) return getRange(to, from);
-   return Array.from({length: to-from+1} , (v, k) => k+from)
+  if (from === to) return [from];
+  if (from > to) return getRange(to, from);
+  return Array.from({length: to - from + 1}, (v, k) => k + from);
 };
 const getRanges = function() {
-  return Array.from(arguments).map(
-     ({from, to}) => getRange(from, to)
-    ).reduce((a, b) => [...a, ...b] , []);
+  return Array.from(arguments)
+    .map(({from, to}) => getRange(from, to))
+    .reduce((a, b) => [...a, ...b], []);
 };
 
 describe(`Progress`, () => {
-
   it(`wraps errors`, () => {
     expect(Progress.throwError('')).toBeAn(Error);
   });
   it(`renders two nested DIV`, () => {
-    const wrapper = mount( <Progress /> );
+    const wrapper = mount(<Progress />);
     expect(wrapper.find('div').length).toEqual(2);
   });
 
@@ -48,15 +44,13 @@ describe(`Progress`, () => {
 
   describe(`completeness`, () => {
     it(`renders by default with 0% of completeness`, () => {
-      const wrapper = mount( <Progress /> );
+      const wrapper = mount(<Progress />);
       expect(wrapper.prop('completed')).toEqual(0);
     });
     it(`does not accept completeness not a number`, () => {
       const spy = sinon.stub(Progress, 'throwError').returns(false),
-      anyThingNotNumber = ['100', '54', [43], new Object(), {something:'3'}, new Date()];
-      anyThingNotNumber.forEach(
-        (value) => <Progress completed = {value} />
-      );
+        anyThingNotNumber = ['100', '54', [43], new Object(), {something: '3'}, new Date()];
+      anyThingNotNumber.forEach(value => <Progress completed={value} />);
       expect(spy.callCount).toEqual(anyThingNotNumber.length);
       spy.restore();
     });
@@ -64,23 +58,19 @@ describe(`Progress`, () => {
       const spy = sinon.stub(Progress, 'throwError').returns(false);
       const rangesOutOf_0_100 = getRanges({from: 101, to: 250}, {from: -1, to: -100});
 
-      rangesOutOf_0_100.forEach(
-        (value) => <Progress completed = {value} />
-      );
+      rangesOutOf_0_100.forEach(value => <Progress completed={value} />);
 
       expect(spy.callCount).toEqual(rangesOutOf_0_100.length);
       spy.restore();
     });
 
     it(`reflects completeness on the width of UI`, () => {
-      const anyPercentage= 50;
+      const anyPercentage = 50;
       let wrapper;
-      getRange(0, 100).forEach((value) => {
+      getRange(0, 100).forEach(value => {
         wrapper = mount(<Progress completed={value} />);
-        expect(wrapper.find('.progressbar-progress').get(0).style.width).toEqual(value+'%');
+        expect(wrapper.find('.progressbar-progress').get(0).style.width).toEqual(value + '%');
       });
     });
-
-  })
-
-})
+  });
+});
